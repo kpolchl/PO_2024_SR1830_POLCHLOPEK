f8de1a8d450a0ae3a9f8eb8d41dce8f8a87f9c4d
kpolchl <kpolchlopek@student.agh.edu.pl>
2024-12-17 14:01:12 +0100

    finished

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -7,11 +7,14 @@ import agh.ics.oop.model.util.Boundary;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.util.List;
 
@@ -34,25 +37,100 @@ public class SimulationPresenter implements MapChangeListener {
 
 
     private WorldMap worldMap;
-    private static Vector2d WIDTH;
-    private static Vector2d HEIGHT;
+
+    private static int WIDTH;
+    private static int HEIGHT;
+    private int xMax;
+    private int yMax;
+    private int xMin;
+    private int yMin;
+    private static int CELL_WIDTH = 30;
+    private static int CELL_HEIGHT = 30;
 
     public void setWorldMap(WorldMap map) {
         this.worldMap = map;
-        Boundary newBounds = map.getCurrentBounds();
-        WIDTH = newBounds.BOTTOM_LEFT();
-        HEIGHT = newBounds.TOP_RIGHT();
     }
 
-    public void drawMap() {
-        mapGrid.add();
+    public void updateBounds() {
 
+
+
+    }
+
+    public void updateMapBounds() {
+        xMin = worldMap.getCurrentBounds().BOTTOM_LEFT().getX();
+        yMin = worldMap.getCurrentBounds().BOTTOM_LEFT().getY();
+        xMax = worldMap.getCurrentBounds().TOP_RIGHT().getX();
+        yMax = worldMap.getCurrentBounds().TOP_RIGHT().getY();
+        WIDTH = xMax - xMin+1;
+        HEIGHT = yMax - yMin+1;
+    }
+
+    public void xyLabel(){
+        Label label = new Label("y/x");
+        mapGrid.add(label, 0, 0);
+        GridPane.setHalignment(label, HPos.CENTER);
+        mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+        mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+    }
+
+    public void drawRows() { // first row numbers
+        for (int i = 0; i < HEIGHT; i++) {
+            if (mapGrid.getRowConstraints().size() <= i + 1) {
+                mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+            }
+            Label label = new Label(Integer.toString(yMax - i));
+            mapGrid.add(label, 0, i + 1);
+            GridPane.setHalignment(label, HPos.CENTER);
+        }
+    }
+
+    public void drawColumns() { // first column numbers
+        for (int i = 0; i < WIDTH; i++) {
+            if (mapGrid.getColumnConstraints().size() <= i + 1) {
+                mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+            }
+            Label label = new Label(Integer.toString(xMin + i));
+            mapGrid.add(label, i + 1, 0);
+            GridPane.setHalignment(label, HPos.CENTER);
+        }
+    }
+
+
+
+    public void addElements(){
+        for (int i = xMin; i <= xMax; i++) {
+            for (int j = yMax; j >= yMin; j--) {
+                Vector2d pos = new Vector2d(i, j);
+                Node element;
+
+                if (worldMap.isOccupied(pos)) {
+                    element = new Label(worldMap.objectAt(pos).toString());
+                } else {
+                    element = new Label(" "); // Puste miejsce
+                }
+
+                mapGrid.add(element, i - xMin + 1, yMax - j + 1);
+                mapGrid.setHalignment(element, HPos.CENTER);
+            }
+        }
+    }
+
+
+
+    public void drawMap(){
+        updateMapBounds();
+        xyLabel();
+        drawRows();
+        drawColumns();
+        addElements();
+        mapGrid.setGridLinesVisible(true);
     }
 
     @Override
     public void mapChanged(WorldMap worldMap, String message) {
-
         Platform.runLater(() -> {
+            clearGrid();
             drawMap();
             changeData.setText(message);
         });
@@ -65,21 +143,22 @@ public class SimulationPresenter implements MapChangeListener {
     }
 
     public void onSimulationStartClicked(ActionEvent actionEvent) {
-        try {
-            String userInput = input.getText();
-            List<MoveDirection> directions = OptionsParser.parse(userInput.split(" "));
-            AbstractWorldMap recWorld = new RectangularMap(5, 5);
-            List<Vector2d> positions = List.of(new Vector2d(1, 1), new Vector2d(1, 2));
+        new Thread(() -> {
+            try {
+                String userInput = input.getText();
+                List<MoveDirection> directions = OptionsParser.parse(userInput.split(" "));
+                AbstractWorldMap recWorld =  new GrassField(10);//new RectangularMap(6, 6);
+                List<Vector2d> positions = List.of(new Vector2d(1, 1), new Vector2d(1, 2));
 
-            Simulation recSimulation = new Simulation(positions, directions, recWorld);
-            SimulationEngine simulationEngine = new SimulationEngine(List.of(recSimulation));
-            recWorld.addObserver(this); //add observer
-            this.setWorldMap(recWorld);
-            simulationEngine.runSync(); // run simulations
-
-
-        } catch (IllegalArgumentException e) {
-            changeData.setText("Invalid input: " + e.getMessage());
-        }
+                Simulation recSimulation = new Simulation(positions, directions, recWorld);
+                SimulationEngine simulationEngine = new SimulationEngine(List.of(recSimulation));
+                recWorld.addObserver(this); //add observer
+                this.setWorldMap(recWorld);
+                simulationEngine.runSync(); // run simulations
+            } catch (IllegalArgumentException e) {
+                Platform.runLater(() -> changeData.setText("Invalid input: " + e.getMessage()));
+            }
+        }).start();
     }
+
 }
